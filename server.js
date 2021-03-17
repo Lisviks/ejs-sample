e108ee6e9c0c6cb0b0d8b260fafdeac5208623ce
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2021-03-17 16:54:16 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+// Set view engine to ejs
+app.set('view engine', 'ejs');
+
+// index page
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
+
+// about page
+app.get('/about', (req, res) => {
+  res.render('/pages/about');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
